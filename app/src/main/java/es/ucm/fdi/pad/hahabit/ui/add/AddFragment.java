dfed2c39c49ac743c5557f80dca86664923e3258
iXian <147181020+isaXCC@users.noreply.github.com>
2025-11-23 17:29:04 +0100

    añadir habito

--- app/src/main/java/es/ucm/fdi/pad/hahabit/ui/add/AddFragment.java
+++ app/src/main/java/es/ucm/fdi/pad/hahabit/ui/add/AddFragment.java
@@ -5,10 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.RadioGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
@@ -17,159 +14,211 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.button.MaterialButton;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import es.ucm.fdi.pad.hahabit.R;
+import es.ucm.fdi.pad.hahabit.data.Habit;
 import es.ucm.fdi.pad.hahabit.databinding.FragmentAddBinding;
 
 public class AddFragment extends Fragment {
 
-    private RadioGroup frequencyGroup;
-    private LinearLayout layoutDiasSemana;
-    private LinearLayout layoutIntervalo;
-    private EditText editTextDias;
-
-    // Botones de días de la semana
-    private MaterialButton btnLunes, btnMartes, btnMiercoles, btnJueves;
-    private MaterialButton btnViernes, btnSabado, btnDomingo;
-    private List<MaterialButton> botonesSeleccionados;
     private FragmentAddBinding binding;
+    private AddViewModel addViewModel;
+
+    //botones con el día de la semana
+    private Map<MaterialButton, Integer> weekDayBtns = new HashMap<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        AddViewModel addViewModel =
-                new ViewModelProvider(this).get(AddViewModel.class);
 
+        addViewModel =
+                new ViewModelProvider(this).get(AddViewModel.class);
         binding = FragmentAddBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        // Inicializar la lista de botones seleccionados
-        botonesSeleccionados = new ArrayList<>();
-
-        // Inicializar vistas
-        inicializarVistas();
-
-        // Configurar listeners
+        inicializarBotonesSemanales();
         configurarListeners();
-
-        final TextView textView = binding.textAdd;
-        addViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
         return root;
     }
-    private void inicializarVistas() {
-        // Usando binding para acceder a las vistas
-        frequencyGroup = binding.frequencyGroup;
-        layoutDiasSemana = binding.layoutDiasSemana;
-        layoutIntervalo = binding.layoutIntervalo;
-        editTextDias = binding.editTextDias;
 
-        // Inicializar botones de días
-        btnLunes = binding.btnLunes;
-        btnMartes = binding.btnMartes;
-        btnMiercoles = binding.btnMiercoles;
-        btnJueves = binding.btnJueves;
-        btnViernes = binding.btnViernes;
-        btnSabado = binding.btnSabado;
-        btnDomingo = binding.btnDomingo;
+    private void inicializarBotonesSemanales() {
+        weekDayBtns.put(binding.btnLunes, 1);
+        weekDayBtns.put(binding.btnMartes, 2);
+        weekDayBtns.put(binding.btnMiercoles, 3);
+        weekDayBtns.put(binding.btnJueves, 4);
+        weekDayBtns.put(binding.btnViernes, 5);
+        weekDayBtns.put(binding.btnSabado, 6);
+        weekDayBtns.put(binding.btnDomingo, 7);
     }
 
     private void configurarListeners() {
-        // Listener para cambiar entre vistas según el RadioButton seleccionado
-        frequencyGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+        // Area
+        binding.cookingButton.setOnClickListener(v -> addViewModel.setArea("Cocinar"));
+        binding.sportsButton.setOnClickListener(v -> addViewModel.setArea("Deporte"));
+        binding.studyButton.setOnClickListener(v -> addViewModel.setArea("Estudio"));
+        binding.othersButton.setOnClickListener(v -> addViewModel.setArea("Otros"));
+
+        // Tipo
+        binding.standarButton.setOnClickListener(v -> addViewModel.setType("Estandar"));
+        binding.listButton.setOnClickListener(v -> addViewModel.setType("Lista"));
+        binding.timerButton.setOnClickListener(v -> addViewModel.setType("Temporizador"));
+
+        // Frecuncia semanal o intervalo
+        binding.frequencyGroup.setOnCheckedChangeListener((group, checkedId) -> {
+
+            // Por dias de la semana
                 if (checkedId == R.id.weeklyButton) {
-                    // Mostrar vista de días de la semana
-                    layoutDiasSemana.setVisibility(View.VISIBLE);
-                    layoutIntervalo.setVisibility(View.GONE);
-                } else if (checkedId == R.id.daysButton) {
-                    // Mostrar vista de intervalo de días
-                    layoutDiasSemana.setVisibility(View.GONE);
-                    layoutIntervalo.setVisibility(View.VISIBLE);
+
+                    // Se muestra la vista
+                    binding.layoutDiasSemana.setVisibility(View.VISIBLE);
+                    binding.layoutIntervalo.setVisibility(View.GONE);
+
+                    // Se guarda el tipo de frecuencia
+                    addViewModel.setTypeFrequency(0);
                 }
-            }
+
+                // Por intervalo de dias
+                else if (checkedId == R.id.daysButton) {
+
+                    // Se muestra la vista
+                    binding.layoutDiasSemana.setVisibility(View.GONE);
+                    binding.layoutIntervalo.setVisibility(View.VISIBLE);
+
+                    // Se guarda el tipo de fecuencia
+                    addViewModel.setTypeFrequency(0);
+                }
         });
 
-        // Configurar listeners para los botones de días de la semana
-        configurarBotonDia(btnLunes, "Lunes");
-        configurarBotonDia(btnMartes, "Martes");
-        configurarBotonDia(btnMiercoles, "Miércoles");
-        configurarBotonDia(btnJueves, "Jueves");
-        configurarBotonDia(btnViernes, "Viernes");
-        configurarBotonDia(btnSabado, "Sábado");
-        configurarBotonDia(btnDomingo, "Domingo");
-    }
+        // Botones de los días de la semana
+        for (MaterialButton btn : weekDayBtns.keySet()) {
+            btn.setOnClickListener(v -> toggleBotonDia(btn));
+        }
 
-    private void configurarBotonDia(final MaterialButton boton, final String nombreDia) {
-        boton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (botonesSeleccionados.contains(boton)) {
-                    // Deseleccionar - volver al color gris
-                    botonesSeleccionados.remove(boton);
-                    boton.setBackgroundTintList(ColorStateList.valueOf(
-                            ContextCompat.getColor(requireContext(), android.R.color.darker_gray)
-                    ));
-                } else {
-                    // Seleccionar - cambiar a azul
-                    botonesSeleccionados.add(boton);
-                    boton.setBackgroundTintList(ColorStateList.valueOf(
-                            ContextCompat.getColor(requireContext(), android.R.color.holo_blue_light)
-                    ));
-                }
+        // Boton de añadir habito
+        binding.addButton.setOnClickListener(v -> {
+            setStrings();
+            if (validate()) {
+
+                // Se guarda
+                Habit habit = addViewModel.buildHabit();
+
+                System.out.println("Hábito creado: " + habit.getTitle() + " - " + habit.getDaysFrequency());
+                addViewModel.insertHabit(habit);
+
+
+
+                Toast.makeText(requireContext(), "Hábito creado correctamente", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    // Método para obtener la configuración de frecuencia seleccionada
-    public String obtenerConfiguracionFrecuencia() {
-        int checkedId = frequencyGroup.getCheckedRadioButtonId();
+    //  Seleccionar/deseleccionar dia de la semana y avisar al ViewModel
+    private void toggleBotonDia(MaterialButton btn) {
 
-        if (checkedId == R.id.weeklyButton) {
-            // Días de la semana seleccionados
-            if (botonesSeleccionados.isEmpty()) {
-                return "No se han seleccionado días";
-            }
-            StringBuilder dias = new StringBuilder("Días: ");
-            for (MaterialButton btn : botonesSeleccionados) {
-                dias.append(btn.getText()).append(" ");
-            }
-            return dias.toString();
-        } else if (checkedId == R.id.daysButton) {
-            // Intervalo de días
-            String numDias = editTextDias.getText().toString();
-            if (numDias.isEmpty()) {
-                return "No se ha ingresado un intervalo";
-            }
-            return "Cada " + numDias + " días";
+        // Se obtine a que día pertenece el boton
+        int dia = weekDayBtns.get(btn);
+
+        // se guarda en el model
+        addViewModel.setWeekDays(dia);
+
+        // Model devuelve la lista de días de la semana seleccionados
+        boolean seleccionado = addViewModel.getSelectedWeekDays().getValue().contains(dia);
+
+        // Cambia de color dependiendo de si está seleccionado
+        if (seleccionado) {
+            btn.setBackgroundTintList(ColorStateList.valueOf(
+                    ContextCompat.getColor(requireContext(), android.R.color.holo_blue_light)
+            ));
+        } else {
+            btn.setBackgroundTintList(ColorStateList.valueOf(
+                    ContextCompat.getColor(requireContext(), android.R.color.darker_gray)
+            ));
         }
-
-        return "No se ha seleccionado frecuencia";
     }
 
-    // Método para obtener lista de días seleccionados (útil para guardar en BD)
-    public List<String> obtenerDiasSeleccionados() {
-        List<String> dias = new ArrayList<>();
-        for (MaterialButton btn : botonesSeleccionados) {
-            dias.add(btn.getText().toString());
-        }
-        return dias;
-    }
+    private void setStrings(){
 
-    // Método para obtener el intervalo de días (útil para guardar en BD)
-    public int obtenerIntervaloDias() {
-        String texto = editTextDias.getText().toString();
-        if (!texto.isEmpty()) {
+        // Titulo
+        addViewModel.setTitle(binding.titleText.getText().toString());
+
+        // Tiempo
+        /*String timeText = binding.timeText.getText().toString(); // "14:30"
+        String[] strings = timeText.split(":");
+        int hour = Integer.parseInt(strings[0]);
+        int minute = Integer.parseInt(strings[1]);
+        addViewModel.setTime(hour, minute);*/
+
+        String intervalText = binding.editTextDias.getText().toString().trim();
+        int interval = 0; // valor por defecto
+
+        if (!intervalText.isEmpty()) {
             try {
-                return Integer.parseInt(texto);
+                interval = Integer.parseInt(intervalText);
             } catch (NumberFormatException e) {
-                return 0;
+                Toast.makeText(requireContext(), "Introduce un número válido", Toast.LENGTH_SHORT).show();
+                return; // salimos de la función para no continuar con datos inválidos
             }
         }
-        return 0;
+
+        // ahora sí podemos pasarlo al ViewModel
+        addViewModel.setIntervalDays(interval);
     }
+
+    private boolean validate() {
+        // Titulo
+        String title = addViewModel.getTitle().getValue();
+        if (title.isEmpty()) {
+            Toast.makeText(requireContext(), "Por favor, añada un título", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        // Área
+        String area = addViewModel.getArea().getValue();
+        if (area == null || area.isEmpty()) {
+            Toast.makeText(requireContext(), "Por favor, seleccione un área", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        // Tipo
+        String type = addViewModel.getType().getValue();
+        if (type == null || type.isEmpty()) {
+            Toast.makeText(requireContext(), "Por favor, seleccione un tipo", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        // Frecuencia
+        Integer typeFreq = addViewModel.getTypeFrequency().getValue();
+        if (typeFreq == null) {
+            Toast.makeText(requireContext(), "Por favor, seleccione una frecuencia", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        // Si es por intervalo de días
+        if (typeFreq == 1) {
+            Integer interval = addViewModel.getIntervalDays().getValue();
+            if (interval == null) {
+                Toast.makeText(requireContext(), "Por favor, añada un intérvalo de días", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            else if(interval < 0){
+                Toast.makeText(requireContext(), "Por favor, añada un intérvalo de días positivo", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+
+        // Hora
+        /*String time = binding.timeText.getText().toString().trim();
+        if (time.isEmpty()) {
+            Toast.makeText(requireContext(), "Por favor, añada una hora", Toast.LENGTH_SHORT).show();
+            return false;
+        }*/
+
+        return true;
+    }
+
 
     @Override
     public void onDestroyView() {
